9b5c58cae580e4c6d19239966d9d1ad0973a50d0
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2025-04-28 19:00:22 +0530

    initalized forntend and added few reusable component & fixed the alias issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
 import "./App.css";
-import React from "react";
 
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
+      <h1 className=" font-bold text-blue-600">
         Welcome to Expanse Tracker
       </h1>
     </div>
